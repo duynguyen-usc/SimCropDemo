8cd739e79dae508b49b88fb71a99cbdd5fc3e45a
Duy Ngoc Nguyen <dnguyen@opentext.com>
2018-09-14 09:48:21 -0700

    add field class

---
+++ SimCropDemo/SimCropDemo/Field.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimCropDemo
+{
+    public class Field
+    {
+        public string Name;
+        private Crop Crop = new Crop();
+
+        public Field(string name)
+        {
+            Name = name;
+        }
+
+        public void Plant(Crop crop)
+        {
+            Crop = crop;
+            Crop.DatePlanted = DateTime.Now;
+        }
+
+        public string GetCropType()
+        {
+            return "Nothing has been planted";
+        }
+
+
+    }
+}
