59c53988acb73702e3f3cc95531d80875ac0b30c
Duy Ngoc Nguyen <dnguyen@opentext.com>
2018-09-14 18:07:13 -0700

    start console app

--- SimCropDemo/SimCropDemo/Program.cs
+++ SimCropDemo/SimCropDemo/Program.cs
@@ -10,7 +10,16 @@ namespace SimCropServer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Server started.");
 
+            while (true)
+            {
+                Console.WriteLine("Press q to quit.");
+                if (Console.ReadKey(true).Key == ConsoleKey.Q)
+                    break;
+                
+            }
+            
         }
     }
 }
