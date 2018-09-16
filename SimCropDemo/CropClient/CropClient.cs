ac44d9f52390c4b8178cb2bff3dab0077aac8200
Duy Ngoc Nguyen <dnguyen@opentext.com>
2018-09-16 09:45:22 -0700

    Create methods to send commands to server

--- SimCropDemo/CropClient/CropClient.cs
+++ SimCropDemo/CropClient/CropClient.cs
@@ -6,10 +6,39 @@ namespace CropClient
 {
     public class CropClient: SimpleTCP.SimpleTcpClient
     {
-
         public CropClient()
         {
             this.StringEncoder = Encoding.UTF8;
+        }
+
+        public void TestConnection()
+        {
+            Write(ServerCommands.TestConnection.ToString());
+        }
+
+        public void SendGetFieldsCommand()
+        {
+            Write(ServerCommands.GetFields.ToString());
+        }
+
+        public void SendGetInfoAllFiends()
+        {
+            Write(ServerCommands.GetInfoAllFields.ToString());
+        }
+
+        public void SendGetInfoSingleFieldCommand(string fieldName)
+        {
+            Write(ServerCommands.TestConnection.ToString() + "," + fieldName);
+        }
+
+        public void SendPlantCommand(string fieldName)
+        {
+            Write(ServerCommands.Plant.ToString() + "," + fieldName);
+        }
+
+        public void SendHarvestcommand(string fieldName)
+        {
+            Write(ServerCommands.Plant.ToString() + "," + fieldName);
         }
     }
 }
